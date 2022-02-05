dcd9a4afe196fcbb0bb8b90f866e59dc7af2034e
kostya2139 <97086581+kostya2139@users.noreply.github.com>
2022-02-05 17:52:08 +0200

    Create problem H.cpp

---
+++ string1/problem H.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Rome_num
+{
+    static const pair<int, char> Rome_digs[7]
+    {
+        {1, 'I'},
+        {5, 'V'},
+        {10, 'X'},
+        {50, 'L'},
+        {100, 'C'},
+        {500, 'D'},
+        {1000, 'M'}
+    };
+    int num;
+public:
+
+};
+
+int main()
+{
+    string s;
+    cin>>s;
+    return 0;
+}
+
